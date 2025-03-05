0a719a1218504b915c7199a9f0bcde0556265cc6
saniksin <35655944+saniksin@users.noreply.github.com>
2025-03-05 23:09:37 +0100

    Update settings.py

--- data/settings.py
+++ data/settings.py
@@ -1,5 +1,5 @@
 # Кол-во попыток в случае ошибки
-NUMBER_OF_ATTEMPTS = 2
+NUMBER_OF_ATTEMPTS = 10
 
 # Одновременное кол-во асинхронных задач
-ASYNC_TASK_IN_SAME_TIME = 1
+ASYNC_TASK_IN_SAME_TIME = 5
